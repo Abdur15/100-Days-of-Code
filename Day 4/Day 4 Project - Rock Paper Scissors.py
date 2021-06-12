15c167a572515a545540e9cc2cf8cb59228cd903
Abdur Raquib N <nasserabu15@gmail.com>
2021-06-12 23:06:34 +0530

    Day 4 Completed

--- Day 4/Day 4 Project - Rock Paper Scissors.py
+++ Day 4/Day 4 Project - Rock Paper Scissors.py
@@ -28,21 +28,43 @@ scissors = '''
 print("Enter your choice 0 for Rock, 1 for Paper, 2 for Scissors")
 sel = int(input("Enter the number"))
 emoji = [rock,paper,scissors]
-print("Your Selection")
-print(emoji[sel])
-print("Computer Selection")
 import random
 game = [0, 1, 2]
 comp = random.choice(game)
-print(emoji[comp])
 
-if(sel == comp):
+
+if(sel >= 3):
+    print("Invalid Number")
+elif(sel == comp):
+    print("Your Selection")
+    print(emoji[sel])
+    print("Computer Selection")
+    print(emoji[comp])
     print("Game Draw")
+
 elif(sel == 1 and comp == 0):
+    print("Your Selection")
+    print(emoji[sel])
+    print("Computer Selection")
+    print(emoji[comp])
     print("You Won")
+
 elif(sel == 2 and comp == 1):
+    print("Your Selection")
+    print(emoji[sel])
+    print("Computer Selection")
+    print(emoji[comp])
     print("You Won")
+
 elif(sel == 0 and comp == 2):
+    print("Your Selection")
+    print(emoji[sel])
+    print("Computer Selection")
+    print(emoji[comp])
     print("You Win")
 else:
+    print("Your Selection")
+    print(emoji[sel])
+    print("Computer Selection")
+    print(emoji[comp])
     print("You Lose")
