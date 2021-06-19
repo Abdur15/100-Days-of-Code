aedc0e4eceb7198aaf901b6a4e91c728172e3c07
Abdur Raquib N <nasserabu15@gmail.com>
2021-06-19 20:05:20 +0530

    Day 7 Completed

--- Day 7/Day 7 Project - Hangman.py
+++ Day 7/Day 7 Project - Hangman.py
@@ -1,6 +1,11 @@
+import pyautogui
+
 import inputs
 import random
 from inputs import to_str
+#import pyautoguia
+logo = inputs.logo
+print(logo)
 print("!!!!! WELCOME TO HANGMAN !!!!!\nGuess the letter to save a life")
 word_list = inputs.word_list
 stages =inputs.stages
@@ -13,26 +18,30 @@ n = 0
 while n<a:
     display.append("_")
     n +=1
-print(to_str(display))
+print(f"Your Letter is {to_str(display).capitalize()}")
 m=len(stages)
 life = []
 while "_" in display:
     guess = input("Enter your Guess :\n").lower()
     if guess not in word:
         life = stages[m - 1]
-        print(to_str(display))
+        print(f"Your Letter is {to_str(display).capitalize()}")
         print("You lose your life", life)
         m -= 1
         if m == 0:
             print("😐 😐 😐 You Hung the Man 😐 😐 😐 \n!!! You lose !!!")
-            print(f"The word was {word.capitalize()} ")
+            print(f"The word was {word.capitalize()}")
             break
+    elif guess in display:
+        print(f"Your Letter is {to_str(display).capitalize()}")
+        print(f"The letter {guess} is already in the word\nChoose any other letter")
     else:
         for i in range(0, len(word)):
             if guess == word[i]:
                 display[i] = guess
-        print(to_str(display))
+        print(f"Your Letter is {to_str(display).capitalize()}")
         if len(life) > 0:
             print("You lose your life", life)
     if "_" not in display:
         print("!!!! ✨ ✨ ✨ 🎊 🎊 🎊 Congrats You Win 🎊 🎊 🎊 ✨ ✨ ✨ !!!!\n👏 👏 👏")
+    pyautogui.hotkey('=','`')
